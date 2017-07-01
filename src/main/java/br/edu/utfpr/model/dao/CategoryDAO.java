03304665e70b456c9810a4c9f965b41974cadc8c
Lucas Sartori <faltou.criatividade0@gmail.com>
2017-07-01 19:51:39 -0300

    listagem das categorias na tela inicial, falta listar o total de ocorrencias em cada categoria

--- src/main/java/br/edu/utfpr/model/dao/CategoryDAO.java
+++ src/main/java/br/edu/utfpr/model/dao/CategoryDAO.java
@@ -6,11 +6,19 @@
 package br.edu.utfpr.model.dao;
 
 import br.edu.utfpr.model.Category;
+import br.edu.utfpr.util.JPAUtil;
+
+import java.util.List;
 
 /**
  *
  * @author felipe
  */
 public class CategoryDAO extends AbstractDAO<Long, Category>{
-    
+
+    public List<Category> getCategories(){
+        this.entityManager = JPAUtil.getEntityManager();
+        return entityManager.createQuery("select o.category.name, count(o) from Occurrence o where o.category = :param")
+           .getResultList();
+    }
 }
