1686829b45e09aa87ca18bb3903d08acece62d21
luizfelipebasile <luizfelipebasile@gmail.com>
2017-07-02 22:48:27 -0300

    login funcionando, falta trocar rotas para pretty e melhorar a tela, nao esquecer de trocar o persistence para none

--- src/main/java/br/edu/utfpr/bean/LoginBean.java
+++ src/main/java/br/edu/utfpr/bean/LoginBean.java
@@ -14,6 +14,7 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -50,6 +51,9 @@ public class LoginBean {
     }
 
     public String onClickLogar() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
+        session.invalidate();
         try {
             HttpServletRequest request = (HttpServletRequest) FacesContext.
                     getCurrentInstance().getExternalContext().getRequest();
@@ -63,6 +67,13 @@ public class LoginBean {
         }
 
         return null;
+    }
+
+    public void logoutB() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
+        session.invalidate();
+
     }
 
     public String getUsuario() {
